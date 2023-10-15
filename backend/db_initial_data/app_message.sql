60b49928bf171f8ebc602285a7e9bf420c18a12a
Rustem Diveev <rustem.diveev@gmail.com>
2023-10-15 18:25:52 +0300

    fix: Маленький фикс генерации данных для моделей

--- backend/db_initial_data/app_message.sql
+++ backend/db_initial_data/app_message.sql
@@ -2,12 +2,12 @@ insert into app_message (message_text, dttm, user_id)
 select 
 	md5(random()::text),
 	now() - random() * interval '365 days',
-	1 
+	(select id from app_user where full_name='Генри Чинаски')
 from generate_series(1, 1000);
 
 insert into app_message (message_text, dttm, user_id)
 select 
 	md5(random()::text),
 	now() - random() * interval '365 days',
-	2 
+	(select id from app_user where full_name='Стрелок Роланд') 
 from generate_series(1, 1000);
